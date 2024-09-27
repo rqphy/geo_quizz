81f431f033b2faabd3c7468185a11a0e21b050e3
rqphy <raphael.ferreira@hetic.net>
2024-09-27 16:25:51 +0200

    :sparkles: create header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import "./header.scss"
+import { Link } from "react-router-dom"
+export default function Header() {
+	return (
+		<header>
+			<Link to={"/"}>Home</Link>
+			<Link to={"/game"}>Game</Link>
+		</header>
+	)
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./routes/home/Home.tsx"
 import Game from "./routes/game/Game.tsx"
 import "./index.scss"
+import Header from "./components/Header/Header.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<BrowserRouter>
+			<Header />
 			<Routes>
 				<Route index element={<Home />} />
 				<Route path="game" element={<Game />} />
