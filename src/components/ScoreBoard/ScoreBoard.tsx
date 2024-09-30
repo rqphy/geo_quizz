fd7e303ab382a8d06e51cca5c0cbfbf64b60d7ad
rqphy <raphael.ferreira@hetic.net>
2024-09-30 15:09:55 +0200

    :lipstick: add basic styling on the scoreboard

--- src/components/ScoreBoard/ScoreBoard.tsx
+++ src/components/ScoreBoard/ScoreBoard.tsx
@@ -1,18 +1,20 @@
 import { IPlayer } from "../../types/interfaces"
+import "./scoreboard.scss"
 
 interface IScoreBoardProps {
 	playerList: IPlayer[]
 }
 
 export default function ScoreBoard({ playerList }: IScoreBoardProps) {
-    // sort playerlist by player score from high to low
+	// sort playerlist by player score from high to low
 	playerList.sort((a, b) => b.score - a.score)
 
 	return (
 		<ul className="scoreboard">
+			<h2>Scores</h2>
 			{playerList.map((player: IPlayer) => (
 				<li>
-					{player.name} : {player.score}
+					<p>{player.name}</p> <p>{player.score}</p>
 				</li>
 			))}
 		</ul>
--- src/routes/game/Game.tsx
+++ src/routes/game/Game.tsx
@@ -1,6 +1,7 @@
 import Quizz from "../../components/Quizz/Quizz"
 import ScoreBoard from "../../components/ScoreBoard/ScoreBoard"
 import EuropeFR from "../../data/FR/europe.json"
+import "./game.scss"
 
 const fakePlayerList = [
 	{ name: "toto", score: 12 },
@@ -8,14 +9,14 @@ const fakePlayerList = [
 	{ name: "Jilo", score: 15 },
 	{ name: "Riri", score: 2 },
 	{ name: "Bolto", score: 8 },
-	{ name: "Savte", score: 8 },
+	{ name: "SavteSavteSavte", score: 8 },
 ]
 
 export default function Game() {
 	return (
-		<>
+		<section className="game">
 			<Quizz countriesList={EuropeFR} />
 			<ScoreBoard playerList={fakePlayerList} />
-		</>
+		</section>
 	)
 }
