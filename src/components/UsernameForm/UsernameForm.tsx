b3736833d2fa0d0d13a173e2039cf074e5fd406e
rqphy <raphael.ferreira@hetic.net>
2024-10-20 15:31:05 +0200

    :sparkles: check username validity

--- src/components/UsernameForm/UsernameForm.tsx
+++ src/components/UsernameForm/UsernameForm.tsx
@@ -1,25 +1,47 @@
 import "./usernameform.scss"
 import Button from "../Button/Button"
-import { FormEvent } from "react"
+import { FormEvent, ChangeEvent, useState } from "react"
 
 interface IUsernameForm {
 	onSubmit: (_event: FormEvent<HTMLFormElement>) => void
 }
 
 export default function UsernameForm({ onSubmit }: IUsernameForm) {
+	const [usernameValue, setUsernameValue] = useState<string>("")
+	const [error, setError] = useState<string | null>(null)
+
+	function handleUsernameChange(_event: ChangeEvent<HTMLInputElement>) {
+		const value = _event.target.value
+		// only allow letters
+		const usernameRegex = /^[a-zA-Z]*$/
+
+		if (!usernameRegex.test(value)) {
+			setError("Le nom doit contenir uniquement des lettres (a-z, A-Z).")
+		} else if (value.length < 3 || value.length > 10) {
+			setError("Le nom doit être comprise entre 3 et 10 caractères.")
+		} else {
+			setError(null)
+		}
+		setUsernameValue(value)
+		console.log(error)
+	}
+
 	return (
-		<form className="username" onSubmit={onSubmit}>
+		<form className={`username ${error && "error"}`} onSubmit={onSubmit}>
 			<input
 				type="text"
 				name="username"
 				className="username__text"
 				placeholder="Entrez votre nom ici"
 				autoFocus
-				maxLength={10}
-				minLength={3}
+				// maxLength={10}
+				// minLength={3}
+				onChange={handleUsernameChange}
+				value={usernameValue}
 				required
 			/>
 			<Button label="Valider" className="username__button" />
+			{error && <p className="username__error">{error}</p>}
 		</form>
 	)
 }
