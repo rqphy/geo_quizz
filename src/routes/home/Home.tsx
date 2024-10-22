eb44ad3f1d4378b9e1d03887af96cdb6b4cad376
rqphy <raphael.ferreira@hetic.net>
2024-10-22 18:40:10 +0200

    :sparkles: add home hero and generate new room

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,3 +1,30 @@
+import Button from "../../components/Button/Button"
+import { v4 as uuidv4 } from "uuid"
+import { useNavigate } from "react-router-dom"
+import "./hero.scss"
+
 export default function Home() {
-	return <>Hello Home</>
+	const navigate = useNavigate()
+
+	function handlePlayClick() {
+		navigate(`/lobby/${uuidv4()}`)
+	}
+
+	return (
+		<>
+			<section className="hero">
+				<div className="hero__content">
+					<h1 className="hero__title">Bienvenue sur GeoQuizz!</h1>
+					<p className="hero__description">
+						Customisez votre quizz et jouez seul ou à plusieurs.
+					</p>
+					<Button
+						label="Jouer"
+						className="hero__play"
+						onClick={handlePlayClick}
+					/>
+				</div>
+			</section>
+		</>
+	)
 }
