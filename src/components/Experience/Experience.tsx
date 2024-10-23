ad22b5ab3176981ec660a60ba7b2b348d1fab9d3
rqphy <raphael.ferreira@hetic.net>
2024-10-23 22:19:06 +0200

    :sparkles: add 3d home animation

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,25 +1,33 @@
 import { useFrame } from "@react-three/fiber"
+import { useGLTF } from "@react-three/drei"
 import "./experience.scss"
 import { useRef } from "react"
 import * as THREE from "three"
 
 export default function Experience() {
 	const planetRef = useRef<THREE.Mesh | null>(null)
+	const planet = useGLTF("./earth/scene.gltf")
 
 	useFrame(() => {
 		if (planetRef.current) {
-			planetRef.current.rotation.y += 0.001
-			planetRef.current.rotation.x += 0.0005
+			planetRef.current.rotation.y -= 0.0015
 		}
 	})
 
 	return (
 		<>
-			<ambientLight />
-			<mesh ref={planetRef}>
-				<sphereGeometry args={[2]} />
-				<meshStandardMaterial wireframe color={"blue"} />
-			</mesh>
+			<directionalLight
+				position={[3, 2, 4]}
+				intensity={1}
+				color={"#ffffff"}
+			/>
+			<ambientLight intensity={0.3} />
+			<primitive
+				ref={planetRef}
+				object={planet.scene}
+				scale={0.02}
+				rotation={[0.2 * Math.PI, 0, 0]}
+			/>
 		</>
 	)
 }
