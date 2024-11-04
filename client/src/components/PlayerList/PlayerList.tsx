4248601bd11ea54b5fe94e9c14c9761030f13aa9
rqphy <raphael.ferreira@hetic.net>
2024-11-04 16:04:54 +0100

    :mute: cleaning

--- client/src/components/PlayerList/PlayerList.tsx
+++ client/src/components/PlayerList/PlayerList.tsx
@@ -3,32 +3,27 @@ import { IPlayer } from "../../types/interfaces"
 
 interface IPlayerList {
 	list: IPlayer[]
-	creatorId: string,
-    currentId: string
+	creatorId: string
+	currentId: string
 }
 
-export default function PlayerList({ list, creatorId, currentId }: IPlayerList) {
+export default function PlayerList({
+	list,
+	creatorId,
+	currentId,
+}: IPlayerList) {
 	return (
 		<ul className="playerlist">
 			{list.map((player) => (
-				<li className={`playerlist__player ${player.uuid === currentId && 'playerlist__player--current'}`}>
+				<li
+					className={`playerlist__player ${
+						player.uuid === currentId &&
+						"playerlist__player--current"
+					}`}
+				>
 					{player.name} {player.uuid === creatorId && "⭐"}
 				</li>
 			))}
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
-			<li className="playerlist__player">Lartiste</li>
 		</ul>
 	)
 }
