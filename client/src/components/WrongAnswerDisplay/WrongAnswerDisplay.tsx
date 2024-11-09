f3db2219ca77ce126921cffaefc76130e62856f0
rqphy <raphael.ferreira@hetic.net>
2024-11-09 19:54:02 +0100

    :sparkles: display wrong answers animations

--- client/src/components/WrongAnswerDisplay/WrongAnswerDisplay.tsx
+++ client/src/components/WrongAnswerDisplay/WrongAnswerDisplay.tsx
@@ -4,6 +4,19 @@ import "./wronganswerdisplay.scss"
 interface IAnswer {
 	id: number
 	text: string
+	coords: { left: number; top: number }
+}
+
+function generateRandomCoords() {
+	let sideMargins = 10 // in percentage
+	let topMargins = 10 // in percentage
+
+	let leftCoord =
+		Math.round(Math.random() * (100 - 2 * sideMargins)) + sideMargins
+	let topCoord =
+		Math.round(Math.random() * (100 - 2 * topMargins)) + topMargins
+
+	return { left: leftCoord, top: topCoord }
 }
 
 export default function WrongAnswerDisplay() {
@@ -12,7 +25,10 @@ export default function WrongAnswerDisplay() {
 	function handleClick() {
 		// Create a unique ID for each answer
 		const id = Date.now()
-		setAnswers((prevAnswers) => [...prevAnswers, { id: id, text: "Hello" }])
+		setAnswers((prevAnswers) => [
+			...prevAnswers,
+			{ id: id, text: "Hello", coords: generateRandomCoords() },
+		])
 
 		// Remove the answer after 5s
 		setTimeout(() => {
@@ -25,11 +41,20 @@ export default function WrongAnswerDisplay() {
 	return (
 		<div className="answerdisplay">
 			<p className="answerdisplay__answer">test</p>
-			<div onClick={handleClick}>Click here</div>
+			<div onClick={handleClick} className="answerdisplay__test">
+				Click here
+			</div>
 
 			<div>
 				{answers.map((answer) => (
-					<p className="answerdisplay__answer" key={answer.id}>
+					<p
+						className="answerdisplay__answer"
+						style={{
+							top: `${answer.coords.top}%`,
+							left: `${answer.coords.left}%`,
+						}}
+						key={answer.id}
+					>
 						{answer.text + answer.id}
 					</p>
 				))}
