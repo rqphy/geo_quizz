57a23c3bb5dbd5ddb6f9933fe9d89ab60fa9d6fa
rqphy <raphael.ferreira@hetic.net>
2024-11-12 16:41:17 +0100

    :truck: remove unused routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./routes/home/Home.tsx"
 import Header from "./components/Header/Header.tsx"
-import MultiPlayer from "./routes/multiplayer/MultiPlayer.tsx"
-import SinglePlayer from "./routes/singleplayer/SinglePlayer.tsx"
 import Lobby from "./routes/lobby/Lobby.tsx"
 import { SocketProvider } from "./contexts/SocketManager.tsx"
 
@@ -16,9 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 				<Header />
 				<Routes>
 					<Route index element={<Home />} />
-					<Route path="/singleplayer" element={<SinglePlayer />} />
 					<Route path="/lobby/:lobbyId" element={<Lobby />} />
-					<Route path="/room/:roomId" element={<MultiPlayer />} />
 				</Routes>
 			</BrowserRouter>
 		</React.StrictMode>
