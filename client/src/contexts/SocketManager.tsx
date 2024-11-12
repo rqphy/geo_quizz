65fda7a960b02677e3c157298e776ace47f4c316
rqphy <raphael.ferreira@hetic.net>
2024-11-12 10:32:10 +0100

    :sparkles: add methods to the socket manager

--- client/src/contexts/SocketManager.tsx
+++ client/src/contexts/SocketManager.tsx
@@ -5,8 +5,19 @@ import { ICountry } from "../types/interfaces"
 const socket = io("http://localhost:3000")
 
 // Socket Context
+interface ISocketProviderMethods {
+	createLobby: () => void,
+	requestLobbyAccess: (lobbyId: string) => void,
+	joinLobby: (lobbyId: string, username: string) => void,
+	leaveLobby: (lobbyId: string, userId: string) => void,
+	setupGame: (lobbyId: string, countriesList: ICountry[]) => void,
+	goodAnswer: (lobbyId: string, userId: string) => void,
+	badAnswer: (lobbyId: string, answer: string) => void
+}
+
 interface ISocketContextProps {
 	socket: Socket
+	methods: ISocketProviderMethods
 }
 const SocketContext = createContext<ISocketContextProps | undefined>(undefined)
 
@@ -15,36 +26,32 @@ interface ISocketProviderProps {
 }
 
 export function SocketProvider({ children }: ISocketProviderProps) {
-	function createLobby(): void {
-		socket.emit("createLobby")
-	}
-
-	function requestLobbyAccess(lobbyId: string): void {
-		socket.emit("requestLobbyAccess", lobbyId)
-	}
-
-	function joinLobby(lobbyId: string, username: string): void {
-		socket.emit("joinLobby", lobbyId, username)
-	}
-
-	function leaveLobby(lobbyId: string, userId: string): void {
-		socket.emit("leaveLobby", lobbyId, userId)
-	}
-
-	function setupGame(lobbyId: string, countriesList: ICountry[]): void {
-		socket.emit("setupGame", lobbyId, countriesList)
-	}
-
-	function goodAnswer(lobbyId: string, userId: string): void {
-		socket.emit("goodAnswer", lobbyId, userId)
-	}
-
-	function badAnswer(lobbyId: string, answer: string) {
-		socket.emit("badAnswer", lobbyId, answer)
+	const methods = {
+		createLobby(): void {
+			socket.emit("createLobby")
+		},
+		requestLobbyAccess(lobbyId: string): void {
+			socket.emit("requestLobbyAccess", lobbyId)
+		},
+		joinLobby(lobbyId: string, username: string): void {
+			socket.emit("joinLobby", lobbyId, username)
+		},
+		leaveLobby(lobbyId: string, userId: string): void {
+			socket.emit("leaveLobby", lobbyId, userId)
+		},
+		setupGame(lobbyId: string, countriesList: ICountry[]): void {
+			socket.emit("setupGame", lobbyId, countriesList)
+		},
+		goodAnswer(lobbyId: string, userId: string): void {
+			socket.emit("goodAnswer", lobbyId, userId)
+		},
+		badAnswer(lobbyId: string, answer: string) {
+			socket.emit("badAnswer", lobbyId, answer)
+		}
 	}
 
 	return (
-		<SocketContext.Provider value={{ socket }}>
+		<SocketContext.Provider value={{ socket, methods}}>
 			{children}
 		</SocketContext.Provider>
 	)
