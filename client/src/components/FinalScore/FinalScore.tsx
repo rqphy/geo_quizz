336dd26ffa7e05678f29d5d6d34fe21ed6e485d7
rqphy <raphael.ferreira@hetic.net>
2024-11-18 16:19:54 +0100

    :sparkles: order game ending screen

--- client/src/components/FinalScore/FinalScore.tsx
+++ client/src/components/FinalScore/FinalScore.tsx
@@ -1,43 +1,32 @@
-import './finalscore.scss'
+import "./finalscore.scss"
 import { IPlayer } from "../../types/interfaces"
 
 interface IFinalScoreProps {
 	playerList: IPlayer[]
 }
 
+let players = [
+	{ name: "Alice", score: 42, uuid: "23423423324" },
+	{ name: "Bob", score: 75, uuid: "23423423232" },
+	{ name: "Charlie", score: 52, uuid: "234231113324" },
+	{ name: "Totol", score: 52, uuid: "234231112324" },
+	{ name: "Tata", score: 12, uuid: "226631113324" },
+	{ name: "Key", score: 32, uuid: "234239913324" },
+	{ name: "Cal", score: 25, uuid: "234238813324" },
+	{ name: "Jerem", score: 14, uuid: "234231167324" },
+]
+
 export default function FinalScore({ playerList }: IFinalScoreProps) {
+	const orderedList = players.sort((a, b) => b.score - a.score)
+
 	return (
 		<section className="finalscore">
-			{playerList.map((player) => (
+			{orderedList.map((player) => (
 				<div className="finalscore__player" key={player.uuid}>
 					<p className="finalscore__name">{player.name}</p>
 					<p className="finalscore__score">{player.score}</p>
 				</div>
 			))}
-            <div className="finalscore__player" key={123}>
-					<p className="finalscore__name">Toto</p>
-					<p className="finalscore__score">12</p>
-				</div>
-            <div className="finalscore__player" key={123}>
-					<p className="finalscore__name">wwwwwwwwwwww</p>
-					<p className="finalscore__score">12</p>
-				</div>
-            <div className="finalscore__player" key={123}>
-					<p className="finalscore__name">Toto</p>
-					<p className="finalscore__score">12</p>
-				</div>
-            <div className="finalscore__player" key={123}>
-					<p className="finalscore__name">Toto</p>
-					<p className="finalscore__score">12</p>
-				</div>
-            <div className="finalscore__player" key={123}>
-					<p className="finalscore__name">Toto</p>
-					<p className="finalscore__score">12</p>
-				</div>
-            <div className="finalscore__player" key={123}>
-					<p className="finalscore__name">Toto</p>
-					<p className="finalscore__score">12</p>
-				</div>
 		</section>
 	)
 }
