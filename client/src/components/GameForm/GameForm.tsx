de359a9333881d350fd7d430ec70e80007a2979d
rqphy <raphael.ferreira@hetic.net>
2024-12-03 03:29:30 +0100

    :sparkles: user can now submit only one good answer

--- client/src/components/GameForm/GameForm.tsx
+++ client/src/components/GameForm/GameForm.tsx
@@ -24,6 +24,7 @@ export default function GameForm({ expectedAnswer }: IGameFormProps) {
 	const { lobbyId } = useParams()
 	const [isAnimating, setIsAnimating] = useState<boolean>(false)
 	const { socket, methods } = useSocket()
+	const [isRight, setIsRight] = useState<boolean>(false)
 
 	function startFailAnimation(): void {
 		setIsAnimating(false)
@@ -32,6 +33,7 @@ export default function GameForm({ expectedAnswer }: IGameFormProps) {
 
 	function handleSubmit(_event: React.FormEvent<HTMLFormElement>) {
 		_event.preventDefault()
+		if (isRight) return
 
 		const inputValue: string = (
 			(_event.target as HTMLFormElement)[0] as HTMLInputElement
@@ -41,6 +43,7 @@ export default function GameForm({ expectedAnswer }: IGameFormProps) {
 			// tada + next round + player point
 			if (!lobbyId || !socket.id) return
 			methods.goodAnswer(lobbyId, socket.id)
+			setIsRight(true)
 		} else {
 			// emit answer
 			if (!lobbyId || !socket.id) return
@@ -54,14 +57,21 @@ export default function GameForm({ expectedAnswer }: IGameFormProps) {
 
 	return (
 		<form onSubmit={handleSubmit} className="gameform">
-			<input
-				type="text"
-				placeholder="Répondre ici..."
-				className={isAnimating ? "gameform__animated" : ""}
-				autoFocus
-				required
-			/>
-			<Button label="Valider" />
+			{!isRight && (
+				<>
+					<input
+						type="text"
+						placeholder="Répondre ici..."
+						className={isAnimating ? "gameform__animated" : ""}
+						autoFocus
+						required
+					/>
+					<Button
+						label="Valider"
+						className={`${isRight && "disabled"}`}
+					/>
+				</>
+			)}
 		</form>
 	)
 }
